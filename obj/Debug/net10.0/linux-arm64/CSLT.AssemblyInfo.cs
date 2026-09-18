bd044c9fd77beffd3d1ae3e6e4ccaeea6750c43e
Ky Dang <kydang@MacBook-Pro-cua-Ky.local>
2026-09-18 22:22:28 +0700

    sua1.5thanh2

--- obj/Debug/net10.0/linux-arm64/CSLT.AssemblyInfo.cs
+++ obj/Debug/net10.0/linux-arm64/CSLT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSLT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10bf860ba7f9f25daebedaadf0b90f084eb6a46")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSLT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSLT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
